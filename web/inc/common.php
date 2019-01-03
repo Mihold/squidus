13eb942e846e454f0b04e00e9c2c67333ade7497
Mykhailo Kutsenko <miholdk@gmail.com>
2019-01-03 13:52:11 -0800

    corrected GPL v2 license headers in the files

--- web/inc/common.php
+++ web/inc/common.php
@@ -2,9 +2,8 @@
 # Squidus (c) 2012 Mykhaylo Kutsenko
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation version 2
-# of the License.
+# modify it under the terms of version 2 of the GNU General Public
+# License as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
